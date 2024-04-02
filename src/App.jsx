47d744595947efc7a49033898b45eb1420da16c6
OmWagh2003 <omwagh610@gmail.com>
2024-04-02 23:46:46 +0530

    aditya

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-00 p-4'>React Router</h1>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
